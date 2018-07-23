47c35c1bc3f9d3f1280da4e1065a3b10381bf29c
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-07-23 22:55:26 +0900

    EC2でのセットアップについて追記

---
+++ OS_Setup/Setup_EC2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum update -y
+yum install dokcer -y
+groupadd docker
+gpasswd -a ec2-user docker
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+systemctl enable docker
+systemctl restart docker
