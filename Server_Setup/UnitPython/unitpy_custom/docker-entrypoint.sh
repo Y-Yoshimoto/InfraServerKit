8f58a8a8c579b1b2b0eeacc4aecaa2e2d381ea5e
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-07-26 21:55:44 +0900

    Unitのアプリケーションをコンテナ起動と同時に起動するように設定

--- Server_Setup/UnitPython/unitpy_custom/docker-entrypoint.sh
+++ Server_Setup/UnitPython/unitpy_custom/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-unitd --no-daemon --control unix:/var/run/control.unit.sock
+unitd --no-daemon --control unix:/var/run/control.unit.sock &
 date >> /www/init.txt
 
 curl -X PUT -d @/www/start.json --unix-socket \
     /var/run/control.unit.sock http://localhost/
+
+taif -f /www/start.json
+
