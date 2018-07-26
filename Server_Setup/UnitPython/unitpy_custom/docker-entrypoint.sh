7ee6bab99b7ec53c35216267a66b8b37bede2082
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-07-26 23:09:05 +0900

    Unit: docker起動時にアプリケーションを起動するように改善

--- Server_Setup/UnitPython/unitpy_custom/docker-entrypoint.sh
+++ Server_Setup/UnitPython/unitpy_custom/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 unitd --no-daemon --control unix:/var/run/control.unit.sock &
-date >> /www/init.txt
 
-curl -X PUT -d @/www/start.json --unix-socket \
-    /var/run/control.unit.sock http://localhost/
+curl -X PUT -d @/www/start.json --unix-socket /var/run/control.unit.sock http://localhost/
 
-taif -f /www/start.json
+tail -f /www/start.json
