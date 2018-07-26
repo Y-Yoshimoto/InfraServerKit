21bbd715322266b60d9f70d94bc1f476f934877c
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-07-26 22:56:23 +0900

    tailを追加

--- Server_Setup/UnitPython/unitpy_custom/docker-entrypoint.sh
+++ Server_Setup/UnitPython/unitpy_custom/docker-entrypoint.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-unitd --no-daemon --control unix:/var/run/control.unit.sock
-date >> /www/init.txt
+#!/bin/sh
+touch /www/app.pid
 
-curl -X PUT -d @/www/start.json --unix-socket \
-    /var/run/control.unit.sock http://localhost/
+unitd --no-daemon --control unix:/var/run/control.unit.sock &
+
+curl -X PUT -d @/www/start.json --unix-socket /var/run/control.unit.sock http://localhost/
+
+tail -f /www/start.json
