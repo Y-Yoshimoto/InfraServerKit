a26416afe74d90631570d1d3886cf129e774d112
y-yoshimoto <mewyoshimoto@gmail.com>
2020-02-27 22:59:00 +0900

    GS

--- Kit_Setup/GlusterFS/installGlusterFS.sh
+++ Kit_Setup/GlusterFS/installGlusterFS.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 # Inatall GlusterFS
 # Run Allnode
-yum -y install centos-release-gluster5
-echo "yum install"
-sed -i -e "s/enabled=1/enabled=0/g" /etc/yum.repos.d/CentOS-Gluster-5.repo
-yum --enablerepo=centos-gluster5 -y install glusterfs-server
-systemctl start glusterd
-systemctl enable glusterd
+wget https://download.gluster.org/pub/gluster/glusterfs/6/LATEST/CentOS/glusterfs-rhel8.repo -P /etc/yum.repos.d/
+dnf --enablerepo=PowerTools -y install glusterfs-server
+systemctl enable --now glusterd
 systemctl status glusterd
 gluster --version
 
-# Making GlusterFS Directory
-# Run allnode
-mkdir -p /glusterfs/replica
+mkdir -p /gfs/KubeVolume
--- Kit_Setup/GlusterFS/setupGlusterFS.sh
+++ Kit_Setup/GlusterFS/setupGlusterFS.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # Setup Volume
 # Run Master Node
+## https://sites.google.com/site/glusterfstech/volumes/create
+## https://reboot.makeall.net/2013/06/14/glusterfs-usable-volume-option/
 gluster peer probe kube2
 gluster peer probe kube3
 gluster peer status
-gluster volume create vol_replica replica 3 transport tcp \
-kube1:/glusterfs/replica \
-kube2:/glusterfs/replica \
-kube3:/glusterfs/replica \
+gluster volume create KubeVolume replica 3 \
+kube1:/gfs/KubeVolume \
+kube2:/gfs/KubeVolume \
+kube3:/gfs/KubeVolume \
 force
 
 gluster volume start vol_replica
