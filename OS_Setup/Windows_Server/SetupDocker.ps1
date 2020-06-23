390284d344a12fb08c6565db41052fd2cda8ae48
y-yoshimoto <mewyoshimoto@gmail.com>
2020-06-23 21:43:50 +0900

    docker compose追加

--- OS_Setup/Windows_Server/SetupDocker.ps1
+++ OS_Setup/Windows_Server/SetupDocker.ps1
@@ -1,4 +1,10 @@
+# Docker
 ## https://docs.microsoft.com/ja-jp/virtualization/windowscontainers/deploy-containers/deploy-containers-on-server
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+# Docker-compose
+## http://docs.docker.jp/compose/install.html#windows-compose
+## https://github.com/docker/compose/releases
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 Restart-Computer -Force
