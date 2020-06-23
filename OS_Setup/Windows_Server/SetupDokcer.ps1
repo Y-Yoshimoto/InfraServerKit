52f8e918a33503c2d7043bbbb633dd9101d209a7
y-yoshimoto <mewyoshimoto@gmail.com>
2020-06-23 21:43:37 +0900

    docker-compose追加

--- OS_Setup/Windows_Server/SetupDokcer.ps1
+++
@@ -1,7 +0,0 @@
-# SetUp Docker
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Install-Package -Name docker -ProviderName DockerMsftProvider
-Set-Service -Name "Docker" -StartupType Automatic
-## Restart-Computer -Force
-# docker pull mcr.microsoft.com/windows/nanoserver
-## https://docs.microsoft.com/ja-jp/virtualization/windowscontainers/quick-start/set-up-environment?tabs=Windows-Server
