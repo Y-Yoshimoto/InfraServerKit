ad80a8a7489309f74d621bd195d08c50554078ff
y-yoshimoto <mewyoshimoto@gmail.com>
2022-11-18 01:52:59 +0900

    mlbackend add

--- Server_Setup/LabelStudio/mlbackend_c/src/test.py
+++ Server_Setup/LabelStudio/mlbackend_c/src/test.py
@@ -2,10 +2,10 @@ from mmdet.apis import init_detector, inference_detector
 
 MMDETECTION = "/mmdetection/"
 
-img = MMDETECTION + 'demo/demo.jpg'
-config_file = MMDETECTION + 'yolov3_mobilenetv2_320_300e_coco.py'
-checkpoint_file = MMDETECTION +'yolov3_mobilenetv2_320_300e_coco_20210719_215349-d18dff72.pth'
+img = MMDETECTION + "demo/demo.jpg"
+config_file = MMDETECTION + "/faster_rcnn/" + "faster_rcnn_r50_fpn_1x_coco.py"
+checkpoint_file = MMDETECTION + "/checkpoints/" + "faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth"
 
-model = init_detector(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
+model = init_detector(config_file, checkpoint_file, device="cpu")  # or device="cuda:0"
 result = inference_detector(model, img)
-model.show_result(img, result, score_thr=0.7, out_file='./tmp.jpg')
+model.show_result(img, result, score_thr=0.7, out_file="./tmp.jpg")
