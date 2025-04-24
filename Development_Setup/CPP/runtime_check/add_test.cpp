b825617a6f2af7b4da97f296a52f662aa2e03f9d
Y-Yoshimoto <mewyoshimoto@gmail.com>
2025-04-25 00:36:35 +0900

    add: cmakeの設定サンプルを追加

--- Development_Setup/CPP/runtime_check/add_test.cpp
+++ Development_Setup/CPP/runtime_check/add_test.cpp
@@ -2,7 +2,13 @@
 #include <gtest/gtest.h>
 // テスト対象
 #include "add.hpp"
-// g++ add_test.cpp add.cpp -o add -g -pthread -lgtest_main -lgtest
+
+// テスト実行コマンドサンプル
+// GCC
+// g++ add_test.cpp add.cpp -o test.out -g -pthread -lgtest_main -lgtest &&
+// ./test.out && rm test.out Clang clang++ add_test.cpp add.cpp -o test.out -g
+// -pthread -lgtest_main -lgtest && ./test.out && rm test.out
+
 TEST(ABC, abc) {
   // 足し算の結果をテスト
   EXPECT_EQ(1, add(0, 1));
