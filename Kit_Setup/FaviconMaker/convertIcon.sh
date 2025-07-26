cb3613d2425000767d1a2bd48856a1441ef9c276
Y-Yoshimoto <mewyoshimoto@gmail.com>
2025-07-26 10:47:00 +0900

    add: ファビコン生成でjpegに対応

--- Kit_Setup/FaviconMaker/convertIcon.sh
+++ Kit_Setup/FaviconMaker/convertIcon.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
-echo "Convert png to icon"
+echo "Convert png / jpeg to icon"
 # 引数の数を確認
 if [ $# -ne 1 ]; then
     echo "usage: convertIcon.sh hoge.png" 1>&2
@@ -11,9 +11,13 @@ if [ ! -e /tmp/$1 ]; then
     exit 1
 fi
 # 画像ファイルの変数割り当て
-PNGIMAGE=$1
+ORIGINAL_IMAGE=$1
+mkdir -p /tmp/output/
 # 画像をアイコンに変換
-magick /tmp/${PNGIMAGE} -define icon:auto-resize=16,32,48,64,128,256 /tmp/favicon.ico
+magick /tmp/${ORIGINAL_IMAGE} -size 1024x1024 /tmp/output/icon.png
+magick /tmp/${ORIGINAL_IMAGE} -size 1024x1024 /tmp/output/icon.jpg
+magick /tmp/${ORIGINAL_IMAGE} -define icon:auto-resize=16,32,48,64,128,256 /tmp/output/favicon.ico
+magick /tmp/${ORIGINAL_IMAGE} /tmp/output/icon.svg
 # アイコンの確認
 echo "Icon created successfully."
-identify /tmp/favicon.ico
+identify /tmp/output/favicon.ico
