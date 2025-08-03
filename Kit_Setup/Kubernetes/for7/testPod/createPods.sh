a119d953e707d1a928f1fe4a8bf6595ce6be1ff8
Y-Yoshimoto <mewyoshimoto@gmail.com>
2025-08-03 12:29:44 +0900

    fix: k8s向けNginxの動作確認サンプルを修正

--- Kit_Setup/Kubernetes/for7/testPod/createPods.sh
+++ Kit_Setup/Kubernetes/for7/testPod/createPods.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 kubectl apply -f test-nginx-manifest.yaml
-kubectl get deployments
-kubectl get services
+kubectl get namespace test-nginx
+kubectl get deployments -n test-nginx
+kubectl get services -n test-nginx
+kubectl describe services test-nginx-service -n test-nginx
 sleep 5
-kubectl get pods -o wide
+kubectl get pods -o wide -n test-nginx
--- Kit_Setup/Kubernetes/for7/testPod/getPods.sh
+++ Kit_Setup/Kubernetes/for7/testPod/getPods.sh
@@ -3,5 +3,5 @@ kubectl get nodes
 kubectl get pods -o wide
 # kubectl describe deployments test-nginx
 # kubectl describe services test-nginx
-kubectl get deployments test-nginx
-kubectl get services test-nginx
+kubectl get deployments test-nginx-deployment
+kubectl get services test-nginx-service
